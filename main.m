236bfb4cf63bc98fead0c3d530cac5d98db04224
erikl <erikl@192.168.0.101>
2021-05-10 11:53:53 +0200

    a) basically klar

--- main.m
+++ main.m
@@ -5,8 +5,8 @@ TI = 1;
 GL = 0;
 
 %element data
-nelm = size(enod,1); % number of elements
 enod=t(1:3,:)'; % nodes of elements
+nelm = size(enod,1); % number of elements
 emat = (t(4,:)==TI)';  %  Material type of element
 
 %node data
@@ -29,7 +29,7 @@ edges_conv = [];
 
 for i = 1:size(er,2)
     if ismember(er(3,i),conv_segments)
-        edges_conv = [edges_conv [er(1:2,i);er(5,i)==1]]; % [node labels of convection elements;convection type]
+        edges_conv = [edges_conv [er(1:2,i);er(3,i)==1]]; % [node labels of convection elements;convection type]
             %convection type = 0 -> T_inf outside
             %convection type = 1 -> T_c outside
     end
@@ -40,14 +40,11 @@ end
 %% a) Stationary temperature distribution
 %Define constants
 T_0 = 20; %base level as T_0 = zero stress.
-T_outside = [40-T_0 20-T_0] % [T_inf T_c]
+T_outside = [40-T_0 20-T_0]; % [T_inf T_c]
 ep = 0.01; % m
 
-D = containers.Map;
-D(TI) = eye(2)*17; %W/(mK)
-D(GL) = eye(2)*0.8; %W/(mK)
-
-alpha_c = 100 %W/(m^2K)
+D = containers.Map({TI,GL},{eye(2)*17, eye(2)*0.8}); %W/(m K)
+alpha_c = 100; %W/(m^2K)
 
 %allocate memory
 K = zeros(nnod,nnod);
@@ -56,8 +53,8 @@ F = zeros(nnod,1);
 %Basic stiffness matrix
 for ie = 1:nelm
     %find coords of element
-    ex = coord(enod(ie,:),1);
-    ey = coord(enod(ie,:),2);
+    ex = coord(enod(ie,:),1)';
+    ey = coord(enod(ie,:),2)';
 
     Ke = flw2te(ex,ey,ep,D(emat(ie)));
     
@@ -83,22 +80,34 @@ for ib = 1:length(edges_conv)
     ey = coord(indx,2);
     l = sqrt((ex(1)-ex(2))^2+(ey(1)-ey(2))^2);  %calculate edge length
     
-    fce = fce_const*l*T(edges_conv(3,ib));
+    material = edges_conv(3,ib)+1;
+    fce = fce_const*l*T_outside(material);
     Kce = Kce_const*l;
 
     K(indx,indx) = K(indx,indx)+Kce;  % insert
     F(indx) = F(indx) + fce;
 end
 
-%Solve system, bc = [dof (=node number tror jag....) temp]
-%Oklart vad vi har här men kanske T_inf vänstra boundary och T_c högra?
-%eller bara högra?
-bc = [???????????];
-T = solveq(K,F,bc);
-
-ex = coord(:,1);
-ey = coord(:,2);
+%Solve system
+T = solveq(K,F)+T_0;
+[ex, ey] = coordxtr(edof, coord, dof, 3);
 ed = extract(edof, T);
-patch(ex',ey',ed');
+
+%Plot
+patch(ex',ey',ed','EdgeColor','none');
+hold on
+patch(ex',-ey',ed','EdgeColor','none');
+
+caxis([-100 50]);
+axis([-0.1 1.2 -0.5 0.5]);
+title("Temperature distribution, T_{\infty} = " + (T_outside(1)+T_0) + " [C]");
+%colormap(cold);
+colorbar;
+xlabel('x-position [m]')
+ylabel('y-postition [m]');
+
+disp("MAX TEMP: " + max(max(ed)));
+%Max T vid T_inf = -96: 19,9769
+%Max T vid T_inf = 40: 40,0093
 
 
